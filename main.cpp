5ac4565fd02ebba8428e12f7b0ac27a7556d7903
Ryan Kennedy <hello@ryankennedy.io>
2016-09-26 21:54:45 +0930

    Rough function to price a forward contract. Should make a class for that, and add unit tests.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <math.h>
+
+enum InstrumentType {
+  forward_contract,
+  call,
+  put,
+  binary_call,
+  binary_put,
+  zero_coupon_bond
+};
+
+float get_price(
+    InstrumentType ins_type,
+    int T,        // Time to maturity.
+    float r,      // Continuously compounding rate.
+    float d,      // Dividend rate.
+    float K,      // Strike price.
+    float S,      // Spot price.
+    float sigma   // Volatility.
+  ) {
+
+  /*
+   * Forward contract pricing.
+   * price = S * e^(rT) - sum( d[i] * e^(r(T-t[i])) )
+   * Assumes dividend is continuous.
+   */
+  if (ins_type == forward_contract) {
+    return S * exp(r * T) - ((S*d) * exp(r * T) );
+  }
+
+  return 0.00;
+}
+
+int main(int argc, char const *argv[]) {
+  float fwd_price = get_price(forward_contract, 1, 0.05, 0.02, 90, 100, 0.1);
+  std::cout << "fwd: " << fwd_price << std::endl;
+  return 0;
+}
