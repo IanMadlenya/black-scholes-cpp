075f86710293249b81e2c7e2f72781b59c6736ea
Ryan Kennedy <hello@ryankennedy.io>
2016-10-02 18:48:33 +1100

    Added some helper functions needed for option pricing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <math.h>
 
-class DerivativePricer {
-  enum InstrumentType {
-    forward_contract,
-    call,
-    put,
-    binary_call,
-    binary_put,
-    zero_coupon_bond
-  };
+enum InstrumentType {
+  forward_contract,
+  call,
+  put,
+  binary_call,
+  binary_put,
+  zero_coupon_bond
+};
 
+class DerivativePricer {
   private:
     InstrumentType ins_type;
-    int T;        // Time to maturity.
-    float r;      // Continuously compounding rate.
-    float d;      // Dividend rate.
-    float K;      // Strike price.
-    float S;      // Spot price.
-    float sigma;  // Volatility.
+    int T;                    // Time to maturity.
+    float r;                  // Continuously compounding rate.
+    float d;                  // Dividend rate.
+    float K;                  // Strike price.
+    float S;                  // Spot price.
+    float sigma;              // Volatility.
+    double func_d(int sign);   // Needed in black-scholes formula. Takes -1 or 1.
+    double cum_norm(double x);  // Cumulative normal function.
 
   public:
     DerivativePricer(InstrumentType ins_type, float T, float r, float d, float K, float S, float sigma);
@@ -35,14 +37,50 @@ DerivativePricer::DerivativePricer(InstrumentType _ins_type, float _T, float _r,
   sigma = _sigma;
 }
 
-float DerivativePricer::getPrice(){
-  return 1.00;
+double DerivativePricer::cum_norm(double x) {
+  const double       A1 = 0.31938153;
+  const double       A2 = -0.356563782;
+  const double       A3 = 1.781477937;
+  const double       A4 = -1.821255978;
+  const double       A5 = 1.330274429;
+  const double RSQRT2PI = 0.39894228040143267793994605993438;
+
+  double K = 1.0 / (1.0 + 0.2316419 * fabs(d));
+
+  double cnd = RSQRT2PI * exp(- 0.5 * d * d) *
+        (K * (A1 + K * (A2 + K * (A3 + K * (A4 + K * A5)))));
+
+  if (d > 0)
+      cnd = 1.0 - cnd;
+
+  return cnd;
+}
+
+double DerivativePricer::func_d(int sign) {
+  return (log(S/K)+(r+((0.5 * sigma * sigma)*sign)*T)) / (sigma*sqrt(T));
+}
+
+float DerivativePricer::getPrice() {
+  /*
+   * Forward contract pricing.
+   */
+  if (ins_type == forward_contract) {
+    return S * exp(r * T) - ((S*d) * exp(r * T) );
+  }
+
+  /*
+   * Call option pricing.
+   */
+  if (ins_type == call) {
+
+  }
+  return 1.0;
 }
 
 
 int main(int argc, char const *argv[]) {
-  // float fwd_price = get_price(forward_contract, 1, 0.05, 0.02, 90, 100, 0.1);
-  // std::cout << "fwd: " << fwd_price << std::endl;
-  std::cout << "yo" << std::endl;
+  DerivativePricer pricer(forward_contract, 1, 0.05, 0.02, 90, 100, 0.1);
+
+  std::cout << pricer.getPrice() << std::endl;
   return 0;
 }
