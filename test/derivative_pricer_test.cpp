688ae531e6610771f84b4f1df745daf48387f740
Ryan Kennedy <hello@ryankennedy.io>
2016-11-02 20:28:02 +1030

    Add test for the special case where a forward contract's price is exactly 0

--- test/derivative_pricer_test.cpp
+++ test/derivative_pricer_test.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <math.h>
 #include "../derivative_pricer.hpp"
 #include "gtest/gtest.h"
 
@@ -8,6 +10,19 @@ class DerivativePricerTest : public ::testing::Test {
   virtual void TearDown() {
   }
 };
+
+// The forward contract will have zero valie iff K=e^((r-d)T)*S
+TEST(DerivativePricerTest,ForwardPriceZeroSpecialCase){
+  double T = 20;
+  double r = 0.05;
+  double d = 0.02;
+  double S = 100;
+  double sigma = 0.1;
+  double K = exp((r-d)*T)*S;
+  DerivativePricer pricer(forward_contract,T,r,d,K,S,sigma);
+  double forward_price = pricer.getPrice();
+  EXPECT_EQ(forward_price, 0.0);
+}
 
 TEST(DerivativePricerTest,PutCallParity){
   DerivativePricer pricer(forward_contract, 2, 0.05, 0.02, 90, 100, 0.05);
@@ -21,3 +36,21 @@ TEST(DerivativePricerTest,PutCallParity){
 
   EXPECT_EQ(forward_price, call_price - put_price);
 }
+
+
+//TODO value parameterized tests AS PER
+//https://github.com/google/googletest/blob/48ee8e98abc950abd8541e15550b18f8f6cfb3a9/googletest/docs/V1_5_AdvancedGuide.md#how-to-write-value-parameterized-tests
+// TEST(DerivativePricerTest, CallDecreasingWithStrike){
+//   int strike = 3;
+//   DerivativePricer pricer(call, 2, 0.05, 0.02, 1, 100, 0.05);
+//   double call_price = pricer.getPrice();
+//   bool is_price_decreasing = true;
+//   while (is_price_decreasing && strike < 10 ) {
+//     std::cout << call_price << std::endl;
+//     is_price_decreasing = (pricer.getPrice() < call_price);
+//     call_price = pricer.getPrice();
+//     strike++;
+//     pricer.setStrike((double) strike);
+//     EXPECT_EQ(true, is_price_decreasing);
+//   }
+// }
